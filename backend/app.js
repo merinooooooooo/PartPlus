46fad4d02694557a70765836afc61a9de4a8c88a
Milton Portillo <miltonfernando0616@gmail.com>
2025-07-08 11:25:00 -0600

    v1

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 import cookieParser from "cookie-parser";
 import clientesRoutes from './src/routes/clientes.js';
 import reservasRoutes from './src/routes/reserva.js';
@@ -9,7 +12,13 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./ricaldone-76f-PartPlus-1.0.0-resolved.json"), "utf-8")
+);
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/clientes", clientesRoutes);
-app.use('/api', reservasRoutes);
+app.use('/api/reservas', reservasRoutes);
 
 export default app;
